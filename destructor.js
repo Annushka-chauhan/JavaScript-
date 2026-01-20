4ed9d9fe2709beb25201a2b25c81a248a84c75c2
anushka chauhan <anushkachauhanannu@gmail.com>
2026-01-20 10:24:58 +0530

    updated file

---
+++ destructor.js
@@ -0,0 +1,23 @@
+//Instead of creating name for each array element at the rhs its the array and at the lsh we have an array which have the list of names for each array index element 
+const [firstName, lastName]=["Anu","Chauhan"];
+console.log(firstName);
+console.log(lastName);
+
+
+//For objects for modern js 
+// const { name, age } = ({
+//     name: "anu",
+//     age: 20
+// });
+
+// console.log(name); // anu
+
+// console.log("JS running");
+
+// const obj = { name: "anu", age: 20 };
+// const { name } = obj;
+
+// console.log(name);
+
+
+
